5a570fc35dd6fa2bf0daa3c9102802a070edd7d9
mohamed magdy <m.magdy05@gmail.com>
2020-06-17 10:01:59 +0300

    Add Project files Structure

---
+++ DatingAppCleanArch.Persistence/DatingAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingAppCleanArch.Persistence
+{
+    class DatingAppContext : DbContext
+    {
+        public DatingAppContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
